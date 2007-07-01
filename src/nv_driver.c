4ff01787a6640bf76cc34a51c99227845dc4678e
Aaron Plattner <aplattner@nvidia.com>
2007-06-30 21:52:08 -0700

    GeForce 8400M G.

--- src/nv_driver.c
+++ src/nv_driver.c
@@ -347,6 +347,7 @@ static SymTabRec NVKnownChipsets[] =
   { 0x10DE0421, "GeForce 8500 GT" },
   { 0x10DE0422, "GeForce 8400 GS" },
   { 0x10DE0423, "GeForce 8300 GS" },
+  { 0x10DE0428, "GeForce 8400M G" },
 
   {-1, NULL}
 };
